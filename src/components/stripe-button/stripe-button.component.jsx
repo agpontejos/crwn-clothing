42f59cdd23eb63c30bf43315060b4222fc7e7831
Armand Pontejos <agpontejos@gmail.com>
2020-03-22 05:16:23 +0800

    udating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_x1PeJf4LRjR6742zNVa2msOY00AWb3wC6l";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN CLothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your Total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
